86b5720028ec72913a128c2f58ed21ad635b5328
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-07-02 00:35:47 +0300

    add propTypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
+import PropTypes from 'prop-types';
 
-// import PropTypes from 'prop-types';
 // import css from './Profile.module.css';
 
 
@@ -35,6 +35,14 @@ export default function Profile({ username, tag, location, avatar, stats }) {
 </div>
   );
 }
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.object,
+};
 
 // ReactDOM.render(
 //     <Profile
