78bc7c3b5e43dd61a67648b139b1ba3516d474a4
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-07-03 00:19:22 +0300

    import all components & json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-import { Profile } from 'components/Profile/Profile';
+import {
+  Profile,
+  FriendList,
+  FriendListItem,
+  StatisticItem,
+  Statistics,
+  TransactionHistory,
+} from 'components/Profile/Profile';
+
 import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
+
 export const App = () => {
   return (
     <div
