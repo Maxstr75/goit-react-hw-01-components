1e2362d628a6fa70ab0f4a8166b30ca32aed5910
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-07-02 23:46:26 +0300

    add task 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Profile } from './components/Profile/Profile';
-import user from './data/user.json';
+import { Profile } from 'components/Profile/Profile';
+import user from 'data/user.json';
 export const App = () => {
   return (
     <div
