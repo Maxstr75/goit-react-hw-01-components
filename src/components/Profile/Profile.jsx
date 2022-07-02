d5000e6de2f1a0138edee510a0ac6bcbe15e8bb6
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-07-02 20:48:20 +0300

    add data

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,59 +2,42 @@ import PropTypes from 'prop-types';
 
 // import css from './Profile.module.css';
 
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt="User avatar" class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
 
-export default function Profile({ username, tag, location, avatar, stats }) {
-    
-    return (
-        <div class="profile">
-  <div class="description">
-    <img
-      src={avatar}
-      alt="User avatar"
-      class="avatar"
-    />
-    <p class="name">{username}</p>
-    <p class="tag">{tag}</p>
-    <p class="location">{location}</p>
-  </div>
-
-  <ul class="stats">
-  <li>
-            <span class="label" >Followers   </span>
-      <span class="quantity">5603</span>
-    </li>
-    <li>
-      <span class="label">Views   </span>
-      <span class="quantity">   4827</span>
-    </li>
-    <li>
-        <span class="label" >Likes   </span>
-      <span class="quantity">1308</span>
-    </li>
-  </ul>
-</div>
+      <ul class="stats">
+        <li>
+          <span class="label">Followers </span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views </span>
+          <span class="quantity"> {stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes </span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
   );
-}
-
-Profile.propTypes = {
-  username: PropTypes.string,
-  tag: PropTypes.string,
-  location: PropTypes.string,
-  avatar: PropTypes.string,
-  stats: PropTypes.object,
 };
 
-// ReactDOM.render(
-//     <Profile
-//         username={user.username}
-//         tag={user.tag}
-//         location={user.location}
-//         avatar={user.avatar}
-//         stats={ user.stats.followers }
-//         views={ user.stats.views }
-//         likes={ user.stats.likes } 
-//     />,
-//     document.querySelector('root'),
-// );
-
- 
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.string.isRequired,
+    views: PropTypes.string.isRequired,
+    likes: PropTypes.string.isRequired,
+  }),
+};
